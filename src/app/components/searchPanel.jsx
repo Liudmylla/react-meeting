9496f36788c94eddb2a01551f51f038b391a1231
Liudmylla <liudmyla12@yahoo.fr>
2021-12-16 21:23:57 +0100

    Search panel

---
+++ src/app/components/searchPanel.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
+const SearchPanel = ({ value, onChange }) => {
+    const input = useRef();
+    useEffect(() => {
+        input.current.focus();
+    }, []);
+
+    return (
+        <div className="input-group mb-3">
+            <input
+                ref={input}
+                onChange={onChange}
+                type="text"
+                className="form-control"
+                placeholder="Find..."
+                value={value}
+            />
+        </div>
+    );
+};
+
+SearchPanel.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+export default SearchPanel;
